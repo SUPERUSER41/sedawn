827b08c651b2a4df9bd54ae7dac7543ce444cf7b
Daneil Greaves <daneilgreaves@gmail.com>
2022-06-05 15:55:49 -0400

    add use manufacturer hook

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from 'next';
 import Card from '../../components/card';
+import { useManufacturers } from '../../hooks/useManufacturers';
+import List from '../../components/list';
 
 const Admin: NextPage = () => {
+  const { manufacturers } = useManufacturers([
+    { id: 1, name: 'Toyota', isActive: true },
+    { id: 2, name: 'Honda', isActive: true },
+    { id: 3, name: 'Kia', isActive: true },
+  ]);
+
   return (
-    <div className="flex h-screen  items-center justify-center">
+    <div className="flex h-screen items-center justify-center">
       <Card>
         <Card.Header title="Manufacturer" />
+        <List
+          items={manufacturers}
+          render={(manufacturer) => <>{manufacturer.name}</>}
+        />
       </Card>
     </div>
   );
